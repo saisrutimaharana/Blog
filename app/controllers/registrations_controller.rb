48d420c3591afa8541fc44ad9b5932990aca1bba
saisrutimaharana <maharanasaisruti@gmail.com>
2023-08-26 19:56:51 +0530

    create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,21 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
     def sign_up_params
       params.require(:user).permit(:email, :password, :password_confirmation, :name, :gender, :dob, :avatar)
     end
-  
-    def create
-        @user = Useer.new(sign_up_params)
-        if @user.save
-          sign_in(@user) 
-          redirect_to @user
-        else
-            render 'new'
-          
-        end
-    end
-    private
-
-    def user_params
-      params.require(:user).permit(:name, :gender, :dob, :other_attributes)
-    end
+    
 end
   
+    
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  
+  has_one_attached :avatar
   has_many :blog_posts
   validates :name, presence: true
   validates :gender, presence: true
@@ -10,6 +10,14 @@ class User < ApplicationRecord
 
  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :authentication_keys => [:name]
+
+  def avatar_url
+    if self.avatar.attached?
+      rails.application.routes.url_helpers.rails_blob_path(self.avatar, only_path: true)
+    else
+      nil
+    end
+  end
 
 
 end
