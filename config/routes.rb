3e7a04933b11d654b36aa7ee1107cce3b4575d57
saisrutimaharana <maharanasaisruti@gmail.com>
2023-09-02 16:26:42 +0530

    create blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: 'registrations'
-  } 
+  devise_for :users, controllers: { registrations: 'users' } 
   resources :blog_posts
   root "blog_posts#index"
 end
